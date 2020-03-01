ef027889cbc5d2f2d7a4956157c88af51e78fa4d
ivanhaj1999 <61594242+ivanhaj1999@users.noreply.github.com>
2020-03-01 16:45:54 +0530

    Add files via upload

---
+++ gifts_new_year/src/main/java/magapu/gifts_new_year/Jellybeans.java
@@ -0,0 +1,19 @@
+package magapu.gifts_new_year;
+
+public class Jellybeans extends Sweets{
+
+    String name;
+
+    String type;
+
+    int weight;
+
+    int cost;
+
+    Jellybeans(String name,String type,int weight,int cost){
+
+        super(name,type,weight,cost);
+
+    }
+
+}
---
+++ gifts_new_year/src/main/java/magapu/gifts_new_year/chocolate.java
@@ -0,0 +1,18 @@
+package magapu.gifts_new_year;
+public class chocolate extends Sweets{
+
+    String name;
+
+    String type;
+
+    int weight;
+
+    int cost;
+
+    chocolate(String name,String type,int weight,int cost){
+
+        super(name,type,weight,cost);
+
+    }
+
+}
